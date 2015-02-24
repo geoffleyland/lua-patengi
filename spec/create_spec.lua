c06c8caa169990895a5ddcc32aba40997d82a147
Geoff Leyland <geoff.leyland@incremental.co.nz>
2015-02-24 17:18:54 +1300

    more mucking with travis - trying to get libpq-fe.h and correcting glaring errors in the tests

--- spec/create_spec.lua
+++ spec/create_spec.lua
@@ -3,11 +3,11 @@ local patengi = require("patengi")
 local db_names =
 {
   sqlite3 = "sqlite3:test.db",
-  pgsql = "pgsql:dbname=test,user=pgsql",
+  pgsql = "pgsql:dbname=test,user=postgres",
 }
 
 describe("front", function()
-  local db_name = db_names[os.getenv("DB") or "sqlite3"]
+  local db_name = db_names[os.getenv("DB") or "sqlite3:test.db"]
 
   it("should create a database", function()
       assert.has_no.errors(function() patengi.open(db_name) end)
