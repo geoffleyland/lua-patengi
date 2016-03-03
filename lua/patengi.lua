444be3c6c36e5e81245d205f513c467541b429b9
Geoff Leyland <geoff.leyland@incremental.co.nz>
2016-03-04 11:23:29 +1300

    Added a comment about how we pick the database

--- lua/patengi.lua
+++ lua/patengi.lua
@@ -2,6 +2,7 @@ return
 {
   open = function(arg1, ...)
       local db_module
+      -- Choose the database based on the DB connection URL
       if arg1:match("postgres:") or arg1:match("dbname=") then
         db_module = require("patengi.pgsql")
       else
